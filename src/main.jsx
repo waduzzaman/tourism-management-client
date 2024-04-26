953cebb72156ebbc8ba04bb172d9773b5a8586ad
Waduzzaman <waduzzaman@gmail.com>
2024-04-26 19:55:06 -0400

    initial project setup

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { RouterProvider } from 'react-router-dom';
+import router from './routes/Router';
+// import AuthProvider from './providers/AuthProvider';
+// import {  HelmetProvider } from 'react-helmet-async';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* <HelmetProvider> */}
+    {/* <AuthProvider> */}
+      <RouterProvider router={router}></RouterProvider>
+    {/* </AuthProvider> */}
+    {/* </HelmetProvider>   */}
+  
+  </React.StrictMode>,
+)
---
+++ src/routes/Router.jsx
@@ -0,0 +1,49 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import Root from "../layouts/Root";
+import Home from "../pages/Home/Home";
+import Blogs from "../pages/Blogs/Blogs";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
+import Register from "../pages/Register/Register";
+import Login from "../pages/Login/Login";
+import PrivateRoute from "./PrivateRoute";
+import UserProfile from "../pages/UserProfile/UserProfile";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+
+      {
+        path: "/blogs",
+        element: <Blogs></Blogs>,
+      },
+      
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+
+      {
+        path: "/register",
+        element: <Register></Register>,
+      },
+      {
+        path: "/userprofile",
+        element: (
+          <PrivateRoute>
+            <UserProfile></UserProfile>
+          </PrivateRoute>
+        ),
+      },
+    ],
+  },
+]);
+
+export default router;
